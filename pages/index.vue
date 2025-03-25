7e68ddab6d1f3029411add24fc2ea0a7db0d98dc
Bilge Demirkaya <bilge@chesskid.com>
2025-03-25 12:33:20 +0000

    add redirection

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { authClient } from '~/lib/auth-client'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(async () => {
+  const session = await authClient.getSession()
+
+  // Redirect User to relevant routes
+  if (session.data?.session) {
+    router.replace('/playlists')
+  } else {
+    router.replace('/login')
+  }
+})
+</script>
+
+<template>
+  <div class="text-center mt-10 text-gray-600">
+    Redirecting...
+  </div>
+</template>
