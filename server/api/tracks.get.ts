0b931b35d221c048bd7b8b638643359733308455
Bilge Demirkaya <bilge@chesskid.com>
2025-03-25 18:18:31 +0000

    make tracks api public

--- server/api/tracks.get.ts
+++ server/api/tracks.get.ts
@@ -1,15 +1,20 @@
-import { auth } from "~/lib/auth";
 import { db } from "~/lib/db";
 import type { Track } from "~/types/index.types";
 
-/** Get Tracks based on Playlist ID */
+/**
+ * Public API: No authentication required to view all tracks
+ * Get Tracks based on Playlist ID
+ *
+*/
 export default defineEventHandler(async (event): Promise<Track[]> => {
-  const session = await auth.api.getSession({ headers: event.headers });
+  const query = getQuery(event)
 
-  if (!session) {
-    throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
-  }
+  const limit = parseInt(query.limit as string) || 20
+  const offset = parseInt(query.offset as string) || 0
 
-  const tracks = db.prepare(`SELECT * FROM track`).all() as unknown[];
-  return tracks.map((track) => track as Track);
-});
+  const tracks = db
+    .prepare(`SELECT * FROM Track LIMIT ? OFFSET ?`)
+    .all(limit, offset) as Track[]
+
+  return tracks
+})
